a0fa6d0b8a37f8705ff4e69619153cc8a2eb3839
Mateus Riva <mriva@ime.usp.br>
2017-12-18 22:50:21 -0200

    finalized plots

---
+++ experiments/results/results_plotter.py
@@ -0,0 +1,237 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+experiments = [3,5,10]
+supports = ["eon", "twdm", "both"]
+results = [("Average wait", 3), ("Mean bandwidth usage", 4), ("Percentage of requests blocked", 2)]
+
+# Plotting average wait for TWDM
+for experiment in experiments:
+    file = "cran_twdm{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[3]))
+
+    plt.plot(n_onus, wait, label="TWDM-{}".format(experiment))
+
+plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Average wait for TWDM")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Average wait (s)")
+plt.savefig("Average wait for TWDM.png")
+plt.clf()
+
+# Plotting average wait for EON
+for experiment in experiments:
+    file = "cran_eon{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[3]))
+
+    plt.plot(n_onus, wait, label="EON-{}".format(experiment))
+
+plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Average wait for EON")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Average wait (s)")
+plt.savefig("Average wait for EON.png")
+plt.clf()
+
+# Plotting average wait for both
+for support in ["eon", "twdm"]:
+    for experiment in experiments:
+        file = "cran_{}{}.txt".format(support, experiment)
+        n_onus = []
+        wait = []
+        first_line = True
+        for line in open(file):
+            if first_line:
+                first_line = False
+                continue
+            n_onus.append(int(line.split('\t')[0]))
+            wait.append(float(line.split('\t')[3]))
+
+        plt.plot(n_onus, wait, label="{}-{}".format(support.upper(), experiment))
+
+plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Average wait")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Average wait (s)")
+plt.savefig("Average wait.png")
+plt.clf()
+
+#--------------------------------------------------------
+# Plotting Mean bandwidth usage for TWDM
+for experiment in experiments:
+    file = "cran_twdm{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[4]))
+
+    plt.plot(n_onus, wait, label="TWDM-{}".format(experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Mean bandwidth usage for TWDM")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Mean bandwidth usage (erlang)")
+plt.savefig("Mean bandwidth usage for TWDM.png")
+plt.clf()
+
+# Plotting Mean bandwidth usage for EON
+for experiment in experiments:
+    file = "cran_eon{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[4]))
+
+    plt.plot(n_onus, wait, label="EON-{}".format(experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Mean bandwidth usage for EON")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Mean bandwidth usage (erlang)")
+plt.savefig("Mean bandwidth usage for EON.png")
+plt.clf()
+
+# Plotting Mean bandwidth usage for both
+for support in ["eon", "twdm"]:
+    for experiment in experiments:
+        file = "cran_{}{}.txt".format(support, experiment)
+        n_onus = []
+        wait = []
+        first_line = True
+        for line in open(file):
+            if first_line:
+                first_line = False
+                continue
+            n_onus.append(int(line.split('\t')[0]))
+            wait.append(float(line.split('\t')[4]))
+
+        plt.plot(n_onus, wait, label="{}-{}".format(support.upper(), experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Mean bandwidth usage")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Mean bandwidth usage (erlang)")
+plt.savefig("Mean bandwidth usage.png")
+plt.clf()
+
+
+#--------------------------------------------------------
+# Plotting Percentage of Lost Packets for TWDM
+for experiment in experiments:
+    file = "cran_twdm{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[2]))
+
+    plt.plot(n_onus, wait, label="TWDM-{}".format(experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Percentage of Lost Packets for TWDM")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Lost packets (%)")
+plt.savefig("Percentage of Lost Packets for TWDM.png")
+plt.clf()
+
+# Plotting Percentage of Lost Packets for EON
+for experiment in experiments:
+    file = "cran_eon{}.txt".format(experiment)
+    n_onus = []
+    wait = []
+    first_line = True
+    for line in open(file):
+        if first_line:
+            first_line = False
+            continue
+        n_onus.append(int(line.split('\t')[0]))
+        wait.append(float(line.split('\t')[2]))
+
+    plt.plot(n_onus, wait, label="EON-{}".format(experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Percentage of Lost Packets for EON")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Lost packets (%)")
+plt.savefig("Percentage of Lost Packets for EON.png")
+plt.clf()
+
+# Plotting Percentage of Lost Packets for both
+for support in ["eon", "twdm"]:
+    for experiment in experiments:
+        file = "cran_{}{}.txt".format(support, experiment)
+        n_onus = []
+        wait = []
+        first_line = True
+        for line in open(file):
+            if first_line:
+                first_line = False
+                continue
+            n_onus.append(int(line.split('\t')[0]))
+            wait.append(float(line.split('\t')[2]))
+
+        plt.plot(n_onus, wait, label="{}-{}".format(support.upper(), experiment))
+
+#plt.yscale('log')
+plt.grid(True, "major")
+plt.grid(True, "minor", linestyle='dotted')
+plt.title("Percentage of Lost Packets")
+plt.legend()
+plt.xlabel("Number of ONUs")
+plt.ylabel("Lost packets (%)")
+plt.savefig("Percentage of Lost Packets.png")
+plt.clf()
